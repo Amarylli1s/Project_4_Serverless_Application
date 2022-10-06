4aff5db5e5e69fc44b16daa64e096f477f37de2d
DSdude <ayoubjbili@gmail.com>
2022-10-06 22:54:20 +0100

    Deleted duplicate model from serverless.yml and prevented creating empty todos

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -14,7 +14,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         statusCode: 202,
         headers: {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({
             uploadUrl: URL,
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -19,7 +19,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({
             "item": toDoItem
