7142967814191bd953aa4a00211bc4d54d32ecc7
DSdude <ayoubjbili@gmail.com>
2022-10-06 02:05:31 +0100

    first commit

---
+++ backend/src/helpers/businessLogic.ts
@@ -0,0 +1,80 @@
+import { createTodo, getTodos, deleteTodo, updateTodo, generateUrl } from './dataLayer'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest';
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest';
+import { Key } from 'readline';
+import { deleteBucket } from './fileManagement';
+
+// TODO: Implement businessLogic
+
+interface params {
+	limit: number,
+	nextKey: Key,
+	sort: string
+}
+
+export function getUserTodos(userId: string, params: params) {
+	return new Promise(async (resolve, reject) => {
+		const todo = await getTodos(userId, params);
+
+		if(todo.status !== "ok") reject(todo.data);
+
+		if(todo.data.Count == todo.newLimit) todo.data.Items.pop(); //because we added 1 to the limit, we should remove the excess
+
+		let next: {};
+		if(params.sort == "created"){
+			next = { //get the next key for pagination
+				createdAt: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].createdAt : undefined,
+				todoId: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].todoId : undefined,
+				userId: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].userId : undefined
+			}
+		}
+		else{
+			next = { //get the next key for pagination
+				dueDate: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].dueDate : undefined,
+				todoId: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].todoId : undefined,
+				userId: todo.data.Items.length > 0 ? todo.data.Items[todo.data.Items.length - 1].userId : undefined,
+			}
+		}
+		
+		const newNextKey = todo.data.Count == todo.newLimit ? JSON.stringify(next) as string : undefined;
+		const todos = {
+			items: todo.data.Items,
+			nextKey: encodeURIComponent(newNextKey)
+		}
+		
+		resolve(todos);
+	});
+}
+
+export function createUserTodo(newTodo: CreateTodoRequest, userId: string): Promise <{}> {
+	return new Promise(async (resolve, reject) => {
+		const todo = await createTodo(newTodo, userId);
+		if(todo.status == "ok") resolve(todo.data)
+		reject(todo.data);
+	});
+}
+
+export function deleteUserTodo(todoId: string, userId: string):Promise <{}> {
+	return new Promise(async (resolve, reject) => {
+		const todo = await deleteTodo(todoId, userId);
+		if(todo.status != "ok") reject(todo.data);
+		deleteBucket(todoId);
+		resolve(todo.data);
+	});
+}
+
+export function updateUserTodo(updatedTodo: UpdateTodoRequest, todoId: string, userId: string):Promise <{}> {
+	return new Promise(async (resolve, reject) => {
+		const todo = await updateTodo(updatedTodo, todoId, userId);
+		if(todo.status == "ok") resolve(todo.data)
+		reject(todo.data);
+	});
+}
+
+export function createAttachmentPresignedUrl(todoId: string, userId: string):Promise <{}> {
+	return new Promise(async (resolve, reject) => {
+		const todo = await generateUrl(todoId, userId);
+		if(todo.status == "ok") resolve(todo.data)
+		reject(todo.data);
+	});
+}
---
+++ backend/src/helpers/dataLayer.ts
@@ -0,0 +1,160 @@
+import {getDocumentClient} from "@shelf/aws-ddb-with-xray";
+import dateFormat from 'dateformat';
+import { createLogger } from '../utils/logger';
+import { v4 as uuid}  from 'uuid';
+import { TodoUpdate } from '../models/TodoUpdate';
+import { Key } from 'readline';
+import { getUploadUrl } from './fileManagement';
+
+// TODO: Implement the dataLayer logic
+
+const todoTable = process.env.TODOS_TABLE;
+const createdIndex = process.env.TODOS_CREATED_INDEX;
+const dueDateIndex = process.env.TODOS_DUE_INDEX;
+const bucketName = process.env.ATTACHMENT_S3_BUCKET;
+const region = process.env.REGION;
+
+const DB = getDocumentClient({ //AWS XRAY TRACING
+	ddbParams: {region: region, convertEmptyValues: true},
+	ddbClientParams: {region: region},
+});
+const logger = createLogger('TodosAccess');
+
+const response = {
+	status: "failed",
+	data: {},
+	newLimit: 4
+}
+
+interface params {
+	limit: number,
+	nextKey: Key,
+	sort: string
+}
+
+export async function getTodos(userId: string, params: params) {
+	try {
+		const newLimit = params.limit + 1; //To avoid dynamodb bringing LastEvaluatedKey when no more result
+		
+		const result = await DB.query({
+			TableName: todoTable,
+			IndexName: params.sort == 'dueDate' ? dueDateIndex : createdIndex,
+			KeyConditionExpression: "userId = :userId",
+			ExpressionAttributeValues: {":userId":`${userId}`},
+			ScanIndexForward: params.sort == 'dueDate' ? true : false,
+			Limit: newLimit,
+			ExclusiveStartKey:  params.nextKey
+		}).promise();
+		
+		return{
+			status: "ok",
+			data: result as any,
+			newLimit
+		}
+	} 
+	catch (error) {
+		logger.log('error', error);
+		return response;
+	}
+	
+}
+
+export async function createTodo(todo: {}, userId: string) {
+	try {
+		const todoId = uuid();
+		const date = new Date()
+		const now = dateFormat(date, 'yyyy-mm-dd HH:MM:ss') as string;
+		const new_todo = {
+			todoId,
+			userId,
+			createdAt: now,
+			done: false,
+			attachmentUrl: "",
+			...todo
+		}
+
+		await DB.put({
+			TableName: todoTable,
+			Item: new_todo
+		}).promise();
+		
+		response.data = new_todo
+		response.status = "ok"
+		return response;
+	} 
+	catch (error) {
+		logger.log('error', error);
+		return response;
+	}
+	
+}
+
+export async function deleteTodo(todoId: string, userId: string) {
+	try {
+		await DB.delete({
+			TableName: todoTable,
+			Key: {
+				todoId,
+				userId
+			}
+		}).promise();
+		
+		response.status = "ok"
+		return response;
+	} 
+	catch (error) {
+		logger.log('error', error);
+		return response;
+	}
+	
+}
+
+export async function updateTodo(todo: TodoUpdate, todoId: string, userId: string) {
+	try {
+		await DB.update({
+			TableName: todoTable,
+			UpdateExpression: "set #tit = :val, dueDate = :dueDate, done = :done",
+			ExpressionAttributeValues: {":val": `${todo.name}`, ":dueDate": `${todo.dueDate}`, ":done": `${todo.done}`},
+			ExpressionAttributeNames: {
+				"#tit": "name"
+			},
+			Key: {
+				todoId,
+				userId
+			}
+		}).promise();
+		
+		response.status = "ok"
+		return response;
+	} 
+	catch (error) {
+		logger.log('error', error);
+		return response;
+	}
+	
+}
+
+export async function generateUrl(todoId: string, userId: string) {
+	try {
+		const uploadUrl = getUploadUrl(todoId);
+		const imageUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`;
+
+		await DB.update({
+			TableName: todoTable,
+			UpdateExpression: "set attachmentUrl = :url",
+			ExpressionAttributeValues: {":url": `${imageUrl}`},
+			Key: {
+				todoId,
+				userId
+			}
+		}).promise();
+		
+		response.data = uploadUrl
+		response.status = "ok";
+		return response;
+	} 
+	catch (error) {
+		logger.log('error', error);
+		return response;
+	}
+}
